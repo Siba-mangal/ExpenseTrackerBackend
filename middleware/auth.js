edd7255022f4b0f8052334f8b3c75dab2438aebd
siba.mangal <sibaprasadmangal48@gmail.com>
2023-06-24 21:39:35 +0530

    change

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,13 @@ const authenticate = (req, res, next) => {
     const token = req.header("Authorization");
     console.log(token);
     console.log(token.split(" ")[1]);
-    const token2 = token.split(" ")[1];
+    let token2 = token.split(" ")[0];
+    if (token2 === "Bearer") {
+      token2 = token.split(" ")[1];
+    } else {
+      token2 = token;
+    }
+
     const user = jwt.verify(token2, "secretkey");
     console.log("userID >>>> ", user.signupId);
     User.findByPk(user.signupId).then((user) => {
